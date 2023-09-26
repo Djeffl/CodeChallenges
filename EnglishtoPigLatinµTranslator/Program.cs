70ced9efb68c2cdc975a243acfcbfac01ae6ae70
Jeff Liekens <jeff.liekens@euri.com>
2023-09-26 21:10:56 +0200

    code challenges;

---
+++ EnglishtoPigLatinµTranslator/Program.cs
@@ -0,0 +1,76 @@
+﻿
+//https://edabit.com/challenge/uhsik73PY7Y2XftzG
+
+//Pig latin has two very simple rules:
+
+//If a word starts with a consonant move the first letter(s) of the word, till you reach a vowel, to the end of the word and add "ay" to the end.
+//have ➞ avehay
+//cram ➞ amcray
+//take ➞ aketay
+//cat ➞ atcay
+//shrimp ➞ impshray
+//trebuchet ➞ ebuchettray
+//If a word starts with a vowel add "yay" to the end of the word.
+//ate ➞ ateyay
+//apple ➞ appleyay
+//oaken ➞ oakenyay
+//eagle ➞ eagleyay
+//Write two functions to make an English to pig latin translator. The first function TranslateWord(word) takes a single word and returns that word translated into pig latin. The second function TranslateSentence(sentence) takes an English sentence and returns that sentence translated into pig latin.
+
+//Examples
+Console.WriteLine(TranslateWord("I"));
+
+//TranslateWord("flag") ➞ "agflay"
+Console.WriteLine(TranslateWord("flag"));
+
+//TranslateWord("Apple") ➞ "Appleyay"
+Console.WriteLine(TranslateWord("Apple"));
+
+//TranslateWord("button") ➞ "uttonbay"
+Console.WriteLine(TranslateWord("button"));
+
+//TranslateWord("") ➞ ""
+Console.WriteLine(TranslateWord(""));
+//TranslateSentence("I like to eat honey waffles.") ➞ "Iyay ikelay otay eatyay oneyhay afflesway."
+Console.WriteLine(TranslateSentence("I like to eat honey waffles."));
+//TranslateSentence("Do you think it is going to rain today?") ➞ "Oday ouyay inkthay ityay isyay oinggay otay ainray odaytay?"
+//Notes
+//Regular expressions will help you not mess up the punctuation in the sentence.
+//If the original word or sentence starts with a capital letter, the translation should preserve its case (see examples #2, #5 and #6).
+
+static string TranslateWord(string word)
+{
+    if (word == "") return "";
+
+    var chars = new Queue<char>(word.ToArray());
+
+    var wordArray = word.ToArray();
+
+    var end = "ay";
+    var isCapitalized = char.IsUpper(wordArray[0]);
+
+    for (var i = 0; i < wordArray.Length; i++)
+    {
+        var c = char.ToLower(wordArray[i]);
+        if (new[] { 'a', 'o', 'i', 'e', 'u' }.Contains(c))
+        {
+            if (i == 0) end = "yay";
+            break;
+        }
+
+        chars.Enqueue(char.ToLower(chars.Dequeue()));
+    }
+
+    return chars.Select((x, i) => i == 0 && isCapitalized ? char.ToUpper(x) : x).Aggregate("", (a, b) => a + b) + end;
+}
+
+static string TranslateSentence(string sentence)
+{
+    return sentence
+        .Split(' ')
+        .Select(word =>
+        {
+            return $"{word.ToCharArray().TakeWhile(Char.IsPunctuation).Aggregate("", (a,b) => a+b)}{TranslateWord(word.Where(c => !char.IsPunctuation(c)).Aggregate("", (a,b) => a+b))}{word.ToCharArray().Reverse().TakeWhile(Char.IsPunctuation).Reverse().Aggregate("", (a, b) => a + b)}";
+        })
+        .Aggregate((a, b) => $"{a} {b}");
+}
---
+++ SimplifiedFractions/Program.cs
@@ -0,0 +1,42 @@
+﻿//Create a function that returns the simplified version of a fraction.
+
+//Examples
+//Simplify("4/6") ➞ "2/3"
+
+//Simplify("10/11") ➞ "10/11"
+
+//Simplify("100/400") ➞ "1/4"
+
+//Simplify("8/4") ➞ "2"
+//Notes
+//A fraction is simplified if there are no common factors (except 1) between the numerator and the denominator. For example, 4/6 is not simplified, since 4 and 6 both share 2 as a factor.
+//If improper fractions can be transformed into integers, do so in your code (see example #4).
+
+//https://edabit.com/challenge/3wT3QcDdfvMR3amjc
+
+static string Simplify(string str)
+{
+    var numerator = int.Parse(str.Split('/')[0]);
+    var denominator = int.Parse(str.Split('/')[1]);
+
+    if (numerator == 0) return "0";
+
+    if (denominator == 0) throw new ArgumentException();
+
+    if (numerator == denominator) return "1";
+
+    if (numerator % denominator == 0) return (numerator / denominator).ToString();
+
+    var simplifyingNumbers = Enumerable.Range(1, numerator).ToArray();
+
+    Array.ForEach(simplifyingNumbers, x =>
+    {
+        if (numerator % x == 0 && denominator % x == 0)
+        {
+            numerator /= x;
+            denominator /= x;
+        }
+    });
+
+    return $"{numerator}/{denominator}";
+}
