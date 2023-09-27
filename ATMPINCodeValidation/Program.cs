5f2cdcf8609855a89f7a43113d904dc7c2a70d33
Jeff Liekens <jeff.liekens@euri.com>
2023-09-27 17:05:44 +0200

    Again some code challenges;

---
+++ ATMPINCodeValidation/Program.cs
@@ -0,0 +1,22 @@
+﻿//https://edabit.com/challenge/gBYEFXeD9G2JMZ9dD
+
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+
+//Examples
+//ValidatePIN("1234") ➞ true
+ValidatePIN("1234");
+
+//ValidatePIN("12345") ➞ false
+
+//ValidatePIN("a234") ➞ false
+
+//ValidatePIN("") ➞ false
+//Notes
+//Some test cases contain special characters.
+//Empty strings must return false.
+
+static bool ValidatePIN(string pin)
+{
+    return (pin.Count() == 4 || pin.Count() == 6) && !pin.Where(c => !char.IsDigit(c)).Any();
+}
+
---
+++ PasswordValidation/Program.cs
@@ -0,0 +1,70 @@
+﻿//https://edabit.com/challenge/etT7orqDDXJF2zGYM
+
+//Create a function that validates a password to conform to the following rules:
+
+//Length between 6 and 24 characters.
+//At least one uppercase letter (A-Z).
+//At least one lowercase letter (a-z).
+//At least one digit (0-9).
+//Maximum of 2 repeated characters.
+//"aa" is OK 👍
+//"aaa" is NOT OK 👎
+//Supported special characters:
+//! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; " ' ? < > , .
+//Examples
+using System.Globalization;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine(ValidatePassword("P1zz@"));
+//ValidatePassword("P1zz@") ➞ false
+//// Too short.
+
+Console.WriteLine(ValidatePassword("iLoveYou"));
+//ValidatePassword("iLoveYou") ➞ false
+//// Missing a number.
+
+Console.WriteLine(ValidatePassword("Fhg93@"));
+//ValidatePassword("Fhg93@") ➞ true
+//// OK!
+///
+
+Console.WriteLine(ValidatePassword("Pè7$areLove"));
+
+Console.WriteLine(ValidatePassword("zZ9'?<>,."));
+
+static bool ValidatePassword(string password)
+{
+    //Length between 6 and 24 characters.
+    if (password.Length < 6 || password.Length > 24)
+        return false;
+
+    //At least one uppercase letter (A-Z).
+    if (!password.Where(char.IsUpper).Any())
+        return false;
+
+    //At least one lowercase letter (a-z).
+    if (!password.Where(char.IsLower).Any())
+        return false;
+
+    //At least one digit (0-9).
+    if (!password.Where(char.IsNumber).Any())
+        return false;
+
+    //Maximum of 2 repeated characters.
+    //"aa" is OK 👍
+    //"aaa" is NOT OK 👎
+    if (password.GroupBy(c => c).Where(x => x.Count() > 2).Any())
+        return false;
+
+    //Supported special characters:
+    //! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; " ' ? < > , .
+    var allowedSymbols = "! @ # $ % ^ & * ( ) + = _ - { } [ ] : ; \" ' ? < > , .".ToCharArray().Where(c => !char.IsWhiteSpace(c));
+    if (password.Where(c => !char.IsLetterOrDigit(c) && !allowedSymbols.Where(s => s == c).Any()).Any())
+        return false;
+
+    if (password.Where(x => new[] {'é', 'è'}.Contains(x)).Any())
+        return false;
+
+    return true;
+}
